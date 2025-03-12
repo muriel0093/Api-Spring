0ed161a521c1656b119ec455253267417f1c5b80
MURIEL M VICENTE <97975046+muriel0093@users.noreply.github.com>
2025-03-11 21:52:42 -0300

    primeiro commit

---
+++ src/test/java/dev/las/weaponsSouls/WeaponsSoulsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.las.weaponsSouls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeaponsSoulsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
